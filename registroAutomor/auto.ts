b73a72d6fc2b75ca6eed46d57301a7ad8c6f988a
Juan <juanlopey22@gmail.com>
2024-10-09 21:18:43 -0300

    se le agrego atributos apartes a cada clase

--- registroAutomor/auto.ts
+++ registroAutomor/auto.ts
@@ -1,5 +1,17 @@
 class Auto extends Vehiculo {
-    constructor(patente: string, carnet: string, vtb: number) {
+    private puertas: number;
+
+    constructor(patente: string, carnet: string, vtb: number, puertas: number) {
         super(patente, carnet, vtb);
+        this.puertas = puertas;
     }
+
+    public getPuertas(): number {
+        return this.puertas;
+    }
+
+    public setPuertas(puertas: number): void {
+        this.puertas = puertas;
+    }
+
 }
